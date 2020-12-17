22c9dd63822b406c3799965a763c90e404592860
ENG18CS0276-SIVA PAVAN NAVEEN G <76085660+ENG18CS0276-SIVA-PAVAN-NAVEEN-G@users.noreply.github.com>
2020-12-17 13:43:02 +0530

    Updated
    ENG18CS0276 - Siva Pavan Naveen G

--- debug_exam.py
+++ debug_exam.py
@@ -112,3 +112,47 @@ if __name__ == '__main__':
     print(data1)
     print(data2)
     print(dup)
+    ..........
+    ..........
+    5)a)Input
+      3
+      1  2
+      2  2
+      8  7
+      2
+      3  3
+      2  9
+      The output had to be
+       {1:2,8:7,3:3}
+       [[3:3].[2:9]]
+       {2,[2,9]}
+       But this is not what we get.
+    .......
+    5)b)
+    There was an indentation error due to which it was not being read ,in 53rd else has to be indentent with if condition.
+    .......
+    5)c)
+    i) Input
+       n1=3
+        1  2
+        2  2
+        9  4
+       n2=2
+        3  3
+        1  4
+        expected output:
+            {2:2,9:4,3:3}
+            [[3,3],[1,4]]
+            {1:[2,4]}
+    
+    ii) Input
+        n1=2
+         1  2
+         2  4
+        n2=2
+         3  4
+         2  5
+         expected output:
+                {2:4,3:4}
+                [[3,4],[2,5]]
+                {2,[4,5]}
